30a5dbfe94b4e99f9c17f5e4ffd8f1245ff94a20
Dmytro Anyduck <dmytro.anyduck@gmail.com>
2026-05-20 21:29:57 +0300

    chore: add wasm plugin

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,6 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import wasm from 'vite-plugin-wasm';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	build: { target: 'esnext' },
+	plugins: [sveltekit(), wasm()]
 });
